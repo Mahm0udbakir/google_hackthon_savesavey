b7bc8f1ac3fa9f7bdc4c3facdfbfa48f1018a213
Anas Nasr <anas.nasr132003@gmail.com>
2025-02-28 13:09:37 +0200

    Routers enhancements

--- lib/presentation/screens/servicesScreen/servicesScreen.dart
+++ lib/presentation/screens/servicesScreen/servicesScreen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_icon_class/font_awesome_icon_class.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_hackthon_savesavey/presentation/screens/chatBotScreen/chatBotScreen.dart';
+import 'package:google_hackthon_savesavey/presentation/screens/createCCScreen/createCC.dart';
+import 'package:google_hackthon_savesavey/presentation/widgets/components.dart';
 
-class servicesScreen extends StatelessWidget {
-  const servicesScreen({super.key});
+class ServicesScreen extends StatelessWidget {
+  const ServicesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,13 +31,44 @@ class servicesScreen extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 30),
               children: [
                 appOptions(
-                    "Financial assistance", FontAwesomeIcons.handHoldingDollar, context),
+                  "Financial assistance",
+                  FontAwesomeIcons.handHoldingDollar,
+                  context,
+                      () {
+                    navigateTo(context, ChatBotScreen());
+                    print("Financial assistance clicked");
+                    // Add navigation or action
+                  },/**/
+                ),
                 appOptions(
-                    "Create Credit Card (CC)", FontAwesomeIcons.creditCard, context),
+                  "Create Credit Card (CC)",
+                  FontAwesomeIcons.creditCard,
+                  context,
+                      () {
+                    navigateTo(context, CreateCCScreen());
+                    print("Create Credit Card clicked");
+                    // Add navigation or action
+                  },
+                ),
                 appOptions(
-                    "Create Debit Card (DC)", FontAwesomeIcons.creditCard, context),
+                  "Create Debit Card (DC)",
+                  FontAwesomeIcons.creditCard,
+                  context,
+                      () {
+                        navigateTo(context, CreateCCScreen());
+                        print("Create Debit Card clicked");
+                    // Add navigation or action
+                  },
+                ),
                 appOptions(
-                    "Investments", FontAwesomeIcons.chartLine, context),
+                  "Investments",
+                  FontAwesomeIcons.chartLine,
+                  context,
+                      () {
+                    print("Investments clicked");
+                    // Add navigation or action
+                  },
+                ),
               ],
             ),
           ),
@@ -43,30 +77,30 @@ class servicesScreen extends StatelessWidget {
     );
   }
 
-  Widget appOptions(String title, IconData icon, BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.symmetric(vertical: 10),
-    child: Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 6,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
-      child: ListTile(
-        leading: Icon(icon, color: Colors.black),
-        title: Text(
-          title,
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+  Widget appOptions(String title, IconData icon, BuildContext context, VoidCallback onTap) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 6,
+              offset: const Offset(0, 3),
+            ),
+          ],
         ),
-        onTap: () {},
+        child: ListTile(
+          leading: Icon(icon, color: Colors.black),
+          title: Text(
+            title,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          ),
+          onTap: onTap,
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
